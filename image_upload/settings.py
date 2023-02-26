fd654df4fa030b75a2c745866dcf4a811d4d0d47
Aheli Biswas <ahelimoon@gmail.com>
2023-02-26 19:55:33 +0530

    deploy update

--- image_upload/settings.py
+++ image_upload/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('Meme_Land_Secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- image_upload/wsgi.py
+++ image_upload/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_upload.settings')
 
 application = get_wsgi_application()
+app = application
